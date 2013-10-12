e92be3768da477a81e3dc3d68f017bbfa0d81556
Frijol <ifoundthemeaningoflife@gmail.com>
2013-10-11 20:21:36 -0700

    fixing merge

--- app.js
+++ app.js
@@ -1,46 +1,12 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , User = require('./models/user')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , mongoose = require('mongoose');
-
+var express = require('express');
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res){
+  res.send('hello world');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.get('/olin', function(req, res){
+  res.send('hello olin');
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-// this should really be a post, but we'll cover that later
-// once we get to AJAX
-app.get('/users/new', user.create);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
-
-
-
-
-
-
+app.listen(3000);
